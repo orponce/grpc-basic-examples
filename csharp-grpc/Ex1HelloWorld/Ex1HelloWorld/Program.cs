7773aa595564403362e123a757ad64485df8927e
Oscar Ponce <92955835+orponce@users.noreply.github.com>
2021-11-23 23:18:01 -0500

    Add basic grpc example

---
+++ csharp-grpc/Ex1HelloWorld/Ex1HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Ex1HelloWorld;
+
+var channel = GrpcChannel.ForAddress("http://localhost:50051");
+var client = new Greeter.GreeterClient(channel);
+
+//var response = await client.SayHelloAsync(new HelloRequest { Name = "you" });
+var response = client.SayHello(new HelloRequest { Name = "World" });
+
+Console.WriteLine("Greeter client received: " + response.Message);
